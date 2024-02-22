1639b5a95a807f8eb2f857ca89e42feefb1b1b3a
Rajavikas03 <rajavikas00007@gmail.com>
2024-02-22 20:13:18 +0530

    push

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,202 @@
-// ignore_for_file: file_names, library_private_types_in_public_api, avoid_print
+// // ignore_for_file: file_names, library_private_types_in_public_api, avoid_print
+
+// import 'dart:async';
+// import 'dart:io';
+// // import 'package:android_path_provider/android_path_provider.dart';
+// import 'package:audio_session/audio_session.dart';
+// import 'package:flutter/foundation.dart' show kIsWeb;
+// import 'package:flutter/material.dart';
+// import 'package:flutter_sound/flutter_sound.dart';
+// import 'package:flutter_sound_platform_interface/flutter_sound_recorder_platform_interface.dart';
+// import 'package:gap/gap.dart';
+// import 'package:permission_handler/permission_handler.dart';
+// import 'package:path_provider/path_provider.dart';
+
+// // import 'package:firebase_storage/firebase_storage.dart';
+// // import 'package:firebase_core/firebase_core.dart';
+// // import 'package:flutter_sound/flutter_sound.dart';
+
+// typedef _Fn = void Function();
+
+// //const theSource = AudioSource.voiceUpLink;
+// //const theSource = AudioSource.voiceDownlink;
+
+// const theSource = AudioSource.microphone;
+
+// /// Example app.
+// class SimpleRecorder extends StatefulWidget {
+//   const SimpleRecorder({super.key});
+
+//   @override
+//   _SimpleRecorderState createState() => _SimpleRecorderState();
+// }
+
+// class _SimpleRecorderState extends State<SimpleRecorder> {
+//   final Codec _codec = Codec.aacMP4;
+//   String _mPath = 'tau_file.mp4';
+//   FlutterSoundPlayer? _mPlayer = FlutterSoundPlayer();
+//   FlutterSoundRecorder? _mRecorder = FlutterSoundRecorder();
+//   bool _mPlayerIsInited = false;
+//   bool _mRecorderIsInited = false;
+//   bool _mplaybackReady = false;
+//   String twoDigitMinutes = " ";
+//   String twoDigitSeconds = " ";
+//   String musicPath = "";
+//   // String song =
+//   //     "/Users/rajavikas/My_space/flutter projects/innowave24_audio_apk/assets/song.mp3";
+
+//   @override
+//   void initState() {
+//     _mPlayer!.openPlayer().then((value) {
+//       setState(() {
+//         _mPlayerIsInited = true;
+//       });
+//     });
+
+//     openTheRecorder().then((value) {
+//       setState(() {
+//         _mRecorderIsInited = true;
+//       });
+//     });
+//     super.initState();
+//   }
+
+//   @override
+//   void dispose() {
+//     _mPlayer!.closePlayer();
+//     _mPlayer = null;
+
+//     _mRecorder!.closeRecorder();
+//     _mRecorder = null;
+//     super.dispose();
+//   }
+
+//   // void getAndroidPaths() async {
+//   //   // var alarmsPath = await AndroidPathProvider.alarmsPath;
+//   //   // var dcimPath = await AndroidPathProvider.dcimPath;
+//   //   // var documentsPath = await AndroidPathProvider.documentsPath;
+//   //   // var downloadsPath = await AndroidPathProvider.downloadsPath;
+//   //   // var moviesPath = await AndroidPathProvider.moviesPath;
+//   //   musicPath = await AndroidPathProvider.musicPath;
+//   //   // var notificationsPath = await AndroidPathProvider.notificationsPath;
+//   //   // var picturesPath = await AndroidPathProvider.picturesPath;
+//   //   // var podcastsPath = await AndroidPathProvider.podcastsPath;
+//   //   // var ringtonesPath = await AndroidPathProvider.ringtonesPath;
+//   // }
+
+//   Future<void> openTheRecorder() async {
+//     if (!kIsWeb) {
+//       var status = await Permission.microphone.request();
+//       if (status != PermissionStatus.granted) {
+//         throw RecordingPermissionException('Microphone permission not granted');
+//       }
+//     }
+
+//     // final file = await getTemporaryDirectory();
+//     await _mRecorder!.openRecorder();
+//     _mRecorder!.setSubscriptionDuration(const Duration(milliseconds: 500));
+//     // if (!await _mRecorder!.isEncoderSupported(_codec) && kIsWeb) {
+//     //   _codec = Codec.opusWebM;
+//     // Directory file = await getApplicationDocumentsDirectory();
+//     // try {
+//     // _mPath = 'tau_file.webm';
+//     // } catch (e) {
+//     //   print(e);
+//     // }
+//     // if (!await _mRecorder!.isEncoderSupported(_codec) && kIsWeb) {
+//     //   _mRecorderIsInited = true;
+//     //   return;
+//     // }
+//     // }
+//     final session = await AudioSession.instance;
+//     await session.configure(AudioSessionConfiguration(
+//       avAudioSessionCategory: AVAudioSessionCategory.playAndRecord,
+//       avAudioSessionCategoryOptions:
+//           AVAudioSessionCategoryOptions.allowBluetooth |
+//               AVAudioSessionCategoryOptions.defaultToSpeaker,
+//       avAudioSessionMode: AVAudioSessionMode.spokenAudio,
+//       avAudioSessionRouteSharingPolicy:
+//           AVAudioSessionRouteSharingPolicy.defaultPolicy,
+//       avAudioSessionSetActiveOptions: AVAudioSessionSetActiveOptions.none,
+//       androidAudioAttributes: const AndroidAudioAttributes(
+//         contentType: AndroidAudioContentType.speech,
+//         flags: AndroidAudioFlags.none,
+//         usage: AndroidAudioUsage.voiceCommunication,
+//       ),
+//       androidAudioFocusGainType: AndroidAudioFocusGainType.gain,
+//       androidWillPauseWhenDucked: true,
+//     ));
+
+//     _mRecorderIsInited = true;
+//   }
+
+//   // <----------------------  Here is the code for recording and playback --------------------->
+
+// // Start recording
+//   void record() {
+//     _mRecorder!
+//         .startRecorder(
+//       toFile: _mPath,
+//       codec: _codec,
+//       audioSource: theSource,
+//     )
+//         .then((value) {
+//       setState(() {});
+//     });
+//   }
+
+// // Stop recording
+//   void stopRecorder() async {
+//     await _mRecorder!.stopRecorder().then((value) {
+//       setState(() {
+//         //var url = value;
+//         _mplaybackReady = true;
+//       });
+//     });
+//   }
+
+// // Play Audio
+//   void play() {
+//     assert(_mPlayerIsInited &&
+//         _mplaybackReady &&
+//         _mRecorder!.isStopped &&
+//         _mPlayer!.isStopped);
+//     _mPlayer!
+//         .startPlayer(
+//             fromURI: _mPath,
+//             //codec: kIsWeb ? Codec.opusWebM : Codec.aacADTS,
+//             whenFinished: () {
+//               setState(() {});
+//             })
+//         .then((value) {
+//       setState(() {});
+//     });
+//   }
+
+// // Stop playing
+//   void stopPlayer() {
+//     _mPlayer!.stopPlayer().then((value) {
+//       setState(() {});
+//     });
+//   }
+
+// // Record function
+//   _Fn? getRecorderFn() {
+//     if (!_mRecorderIsInited || !_mPlayer!.isStopped) {
+//       return null;
+//     }
+//     return _mRecorder!.isStopped ? record : stopRecorder;
+//   }
+
+// // Play function
+//   _Fn? getPlaybackFn() {
+//     if (!_mPlayerIsInited || !_mplaybackReady || !_mRecorder!.isStopped) {
+//       return null;
+//     }
+//     return _mPlayer!.isStopped ? play : stopPlayer;
+//   }
 
 import 'dart:async';
-import 'dart:io';
-// import 'package:android_path_provider/android_path_provider.dart';
 import 'package:audio_session/audio_session.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
@@ -10,23 +204,37 @@ import 'package:flutter_sound/flutter_sound.dart';
 import 'package:flutter_sound_platform_interface/flutter_sound_recorder_platform_interface.dart';
 import 'package:gap/gap.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:path_provider/path_provider.dart';
 
-// import 'package:firebase_storage/firebase_storage.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter_sound/flutter_sound.dart';
+/*
+ * This is an example showing how to record to a Dart Stream.
+ * It writes all the recorded data from a Stream to a File, which is completely stupid:
+ * if an App wants to record something to a File, it must not use Streams.
+ *
+ * The real interest of recording to a Stream is for example to feed a
+ * Speech-to-Text engine, or for processing the Live data in Dart in real time.
+ *
+ */
 
+///
 typedef _Fn = void Function();
 
+/* This does not work. on Android we must have the Manifest.permission.CAPTURE_AUDIO_OUTPUT permission.
+ * But this permission is _is reserved for use by system components and is not available to third-party applications._
+ * Pleaser look to [this](https://developer.android.com/reference/android/media/MediaRecorder.AudioSource#VOICE_UPLINK)
+ *
+ * I think that the problem is because it is illegal to record a communication in many countries.
+ * Probably this stands also on iOS.
+ * Actually I am unable to record DOWNLINK on my Xiaomi Chinese phone.
+ *
+ */
 //const theSource = AudioSource.voiceUpLink;
 //const theSource = AudioSource.voiceDownlink;
-
+String twoDigitSeconds = '';
+String twoDigitMinutes = '';
 const theSource = AudioSource.microphone;
 
 /// Example app.
 class SimpleRecorder extends StatefulWidget {
-  const SimpleRecorder({super.key});
-
   @override
   _SimpleRecorderState createState() => _SimpleRecorderState();
 }
@@ -39,11 +247,6 @@ class _SimpleRecorderState extends State<SimpleRecorder> {
   bool _mPlayerIsInited = false;
   bool _mRecorderIsInited = false;
   bool _mplaybackReady = false;
-  String twoDigitMinutes = " ";
-  String twoDigitSeconds = " ";
-  String musicPath = "";
-  // String song =
-  //     "/Users/rajavikas/My_space/flutter projects/innowave24_audio_apk/assets/song.mp3";
 
   @override
   void initState() {
@@ -71,19 +274,6 @@ class _SimpleRecorderState extends State<SimpleRecorder> {
     super.dispose();
   }
 
-  // void getAndroidPaths() async {
-  //   // var alarmsPath = await AndroidPathProvider.alarmsPath;
-  //   // var dcimPath = await AndroidPathProvider.dcimPath;
-  //   // var documentsPath = await AndroidPathProvider.documentsPath;
-  //   // var downloadsPath = await AndroidPathProvider.downloadsPath;
-  //   // var moviesPath = await AndroidPathProvider.moviesPath;
-  //   musicPath = await AndroidPathProvider.musicPath;
-  //   // var notificationsPath = await AndroidPathProvider.notificationsPath;
-  //   // var picturesPath = await AndroidPathProvider.picturesPath;
-  //   // var podcastsPath = await AndroidPathProvider.podcastsPath;
-  //   // var ringtonesPath = await AndroidPathProvider.ringtonesPath;
-  // }
-
   Future<void> openTheRecorder() async {
     if (!kIsWeb) {
       var status = await Permission.microphone.request();
@@ -91,18 +281,10 @@ class _SimpleRecorderState extends State<SimpleRecorder> {
         throw RecordingPermissionException('Microphone permission not granted');
       }
     }
-
-    // final file = await getTemporaryDirectory();
     await _mRecorder!.openRecorder();
-    _mRecorder!.setSubscriptionDuration(const Duration(milliseconds: 500));
     if (!await _mRecorder!.isEncoderSupported(_codec) && kIsWeb) {
       _codec = Codec.opusWebM;
-      // Directory file = await getApplicationDocumentsDirectory();
-      // try {
       _mPath = 'tau_file.webm';
-      // } catch (e) {
-      //   print(e);
-      // }
       if (!await _mRecorder!.isEncoderSupported(_codec) && kIsWeb) {
         _mRecorderIsInited = true;
         return;
@@ -130,9 +312,8 @@ class _SimpleRecorderState extends State<SimpleRecorder> {
     _mRecorderIsInited = true;
   }
 
-  // <----------------------  Here is the code for recording and playback --------------------->
+  // ----------------------  Here is the code for recording and playback -------
 
-// Start recording
   void record() {
     _mRecorder!
         .startRecorder(
@@ -145,7 +326,6 @@ class _SimpleRecorderState extends State<SimpleRecorder> {
     });
   }
 
-// Stop recording
   void stopRecorder() async {
     await _mRecorder!.stopRecorder().then((value) {
       setState(() {
@@ -155,7 +335,6 @@ class _SimpleRecorderState extends State<SimpleRecorder> {
     });
   }
 
-// Play Audio
   void play() {
     assert(_mPlayerIsInited &&
         _mplaybackReady &&
@@ -173,14 +352,14 @@ class _SimpleRecorderState extends State<SimpleRecorder> {
     });
   }
 
-// Stop playing
   void stopPlayer() {
     _mPlayer!.stopPlayer().then((value) {
       setState(() {});
     });
   }
 
-// Record function
+// ----------------------------- UI --------------------------------------------
+
   _Fn? getRecorderFn() {
     if (!_mRecorderIsInited || !_mPlayer!.isStopped) {
       return null;
@@ -188,7 +367,6 @@ class _SimpleRecorderState extends State<SimpleRecorder> {
     return _mRecorder!.isStopped ? record : stopRecorder;
   }
 
-// Play function
   _Fn? getPlaybackFn() {
     if (!_mPlayerIsInited || !_mplaybackReady || !_mRecorder!.isStopped) {
       return null;
